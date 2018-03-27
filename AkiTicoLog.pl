f360288e79ded007479b37e2265edcd72284841c
Anthonny <lap201197@hotmail.com>
2018-03-27 10:57:39 -0600

    Funcionabilidad basica lograda
    En si, adivina si piensa entre 2 personajes de costa rica, falta pulir ciertos detalles como:
    cuando el usuario no sabe la pregunta, y escribe "siguiente", o ingresar provincias de mas de 1 palabra como " San Jose"

--- AkiTicoLog.pl
+++ AkiTicoLog.pl
@@ -1,10 +1,37 @@
 %CODIGO DEL PROYECTO AKITICOLOG
-%CODIGO DEL PROYECTO AKITICOLOG
 %Base, edad, donde vive, bebida favorita xD(random, por el momento)
+% [Nery Brenes,[Nacimiento,Limon],[Dedicacion,
+% atletismo],[Sexo,hombre],[Edad_mayor_25, si]]
+% [Hanna Gabriels,[Nacimiento, Alajuela],[Dedicacion,
+% Boxeo],[Sexo,mujer],[Edad_mayor_25, si]]
 
 % 1.04 (*) Find the number of elements of a list.
+%
+% recordar usar todo en minuscula y separado por espacios, si escribe
+% san jose o provincias de 2 palabras se cae(debo arreglarlo jaja)
 count(0, []).
 count(C, [_|T]) :- count(X, T), C is X + 1.
+% todavia no uso esa funcion, pero puede ser util jaja
+
+
+nery([],"Nery Brenes").
+nery([X|T],R):-X= atletismo,nery(T,R).
+nery([X|T],R):-X= hombre,nery(T,R).
+nery([X|T],R):-X= si,nery(T,R).
+nery([X|T],R):-X= limon,nery(T,R).
+nery([X|T],R):-X= siguiente ,nery(T,R).
+
+%si no se sabe la pregunta, escribir siguiente, para seguir con la siguiente pregunta
+
+
+hanna([],"Hanna Gabriels").
+hanna([X|T],R):-X= boxeo,hanna(T,R).
+hanna([X|T],R):-X= mujer,hanna(T,R).
+hanna([X|T],R):-X= si,hanna(T,R).
+hanna([X|T],R):-X= alajuela,hanna(T,R).
+hanna([X|T],R):-X= siguiente ,hanna(T,R).
+
+
 
 match([H|_],0,H) :-!
     .
@@ -15,37 +42,41 @@ match([_|T],N,H) :-
 % valor,lista, lista[P],P
 miembroPos(X,[X|T],Z,P):-match(T,P,Z).
 miembroPos(X,[_|T],Z,P):-miembroPos(X,T,Z,P).
+
 %estructura de respuesta comun: "nacio en ..."
 nacimiento(N):-
-    write("Donde nació?"),nl,
+    write("1) Donde nació?"),nl,
     read(Inpt),nl,
     atomic_list_concat(L,' ',Inpt),nl,
-    miembroPos(nacio,L,N,1).
-
+    miembroPos(nacio,L,N,1).  
 %estructura de respuesta comun: si o no
-murio(M):-
-    write("Está muerto?"),nl,
+edad_mayor_25(E):-
+    write("2) Tiene más de 25 años(si/no)?"),nl,
     read(Inpt),
     atomic_list_concat(L,' ',Inpt)
-    ,match(L,0,M).
-
-%estructura de respuesta comun: si o no
-es_cantante(C):-
-    write("Es cantante?"),nl,
-    read(Inpt),
-    atomic_list_concat(L,' ',Inpt)
-    ,match(L,0,C).
+    ,match(L,0,E).
 %estructura de respuesta comun: "se dedica al ..."
 deporte(D):-
-    write("A que deporte se dedica(sino practica ninguno, puedes escribir 'nunguno')?"),nl,
+    write("3) A que deporte se dedica principalmente(sino practica ninguno, puedes escribir 'nunguno')?"),nl,
     read(Inpt),nl,
     atomic_list_concat(L,' ',Inpt),nl,
     miembroPos(al,L,D,0).
+%estructura de respuesta comun: " ella/el es mujer/hombre..."
+sexo(S):-
+    write("4) Es hombre o mujer?"),nl,
+    read(Inpt),
+    atomic_list_concat(L,' ',Inpt),
+    miembroPos(es,L,S,0).
+
+revisar(_,_,0).
+revisar(L,R,N):- hanna(L,R),append([],R,L_r), Nn is N-1,revisar(L,L_r,Nn).
+revisar(L,R,N):- nery(L,R),append([],R,L_r), Nn is N-1,revisar(L,L_r,Nn).
+revisar(L,R,N):- Nn is N-1,revisar(L,R,Nn).
 
 
-% guarda en L la lista de respuestas
 iniciar:-
     write("Bienvenido a AkiTicoLog!"),nl,
     write("Piensa en un personaje de costa rica, intentare adivinarlo"),nl,
-    nacimiento(N),murio(M),es_cantante(C),deporte(D),append([],[N,M,C,D],L),write(L).
+    nacimiento(N),edad_mayor_25(E),deporte(D),sexo(S),append([],[N,E,D,S],L),revisar(L,R,2),write("Piensa en "),write(R).
+
 
